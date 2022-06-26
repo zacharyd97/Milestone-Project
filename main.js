3463bd95764f5510f6d0d41765411d09f25202c3
Zachary Dowell <zdowell97@gmail.com>
2022-06-26 14:54:10 -0400

    zombie movement

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 
+
 function newPlayer(url, left, bottom) {
     let player = document.createElement('img')
     player.src = url
@@ -9,4 +10,33 @@ function newPlayer(url, left, bottom) {
     return player
 }
 
+function newZombie (left, bottom) {
+    let zombie = document.createElement('img')
+    zombie.src = 'assets/8-bit-zombie.png'
+    zombie.style.position = 'fixed'
+    zombie.style.left = left + 'px'
+    zombie.style.bottom = bottom + 'px'
+    document.body.append(zombie)
+    return zombie
+}
+function moveZombie(zombie) {
+    console.log(zombie.style.left)
+    let myInterval = setInterval(() => {
+        zombie.style.left = parseInt(zombie.style.left) + 1 + 'px'
+        if(parseInt(zombie.style.left) == 650) {
+            clearInterval(myInterval)
+        }
+    },50)
+    }
+
+
+const testZombie = newZombie(100,100)
+moveZombie(testZombie)
+
+
+
 newPlayer('assets/placeholder.png',650,250)
+
+
+
+
